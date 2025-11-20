0c8e335d4d99e4f5e9d35016eaae136c3838ae8a
ranmadmon <ranmadmon6@gmail.com>
2025-11-20 15:46:33 +0200

    Fix AppConfig for Spring Boot

--- src/main/java/com/ashcollege/AppConfig.java
+++ src/main/java/com/ashcollege/AppConfig.java
@@ -1,75 +1,19 @@
 package com.ashcollege;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
 @Configuration
 @Profile("production")
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
-    @Bean
-    public DataSource dataSource() throws Exception {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
-
-        // *** IMPORTANT ***
-        // Spring will inject these values from environment variables:
-        // SPRING_DATASOURCE_URL
-        // SPRING_DATASOURCE_USERNAME
-        // SPRING_DATASOURCE_PASSWORD
-        dataSource.setJdbcUrl(System.getenv("SPRING_DATASOURCE_URL"));
-        dataSource.setUser(System.getenv("SPRING_DATASOURCE_USERNAME"));
-        dataSource.setPassword(System.getenv("SPRING_DATASOURCE_PASSWORD"));
-
-        dataSource.setMaxPoolSize(20);
-        dataSource.setMinPoolSize(5);
-        dataSource.setIdleConnectionTestPeriod(3600);
-        dataSource.setTestConnectionOnCheckin(true);
-
-        return dataSource;
-    }
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() throws Exception {
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource());
-
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
-        hibernateProperties.put("hibernate.jdbc.batch_size", 50);
-        hibernateProperties.put("hibernate.connection.characterEncoding", "utf8");
-        hibernateProperties.put("hibernate.enable_lazy_load_no_trans", "true");
-
-        sessionFactoryBean.setHibernateProperties(hibernateProperties);
-        sessionFactoryBean.setMappingResources("objects.hbm.xml");
-
-        return sessionFactoryBean;
-    }
-
-    @Bean
-    public HibernateTransactionManager transactionManager() throws Exception {
-        HibernateTransactionManager tx = new HibernateTransactionManager();
-        tx.setSessionFactory(sessionFactory().getObject());
-        return tx;
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
     }
 }
